56366bf68ca6370823edc4f238de47b3a4f21ff2
Marat Iskhakov <marat.iskakov@avalab.io>
2024-01-15 01:17:09 +0500

    update version to 1.2.1

--- mips_ingenic_t31/configs/faceter-camera.sh
+++ mips_ingenic_t31/configs/faceter-camera.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+cronTask="35 2 * * * sleep $(($RANDOM%5+1))m; reboot"
+cronFile="/etc/crontabs/root"
+
+grep 'reboot' $cronFile || echo "$cronTask" >> $cronFile
+
+while true
+do
 faceter-camera 2>&1 | logger -t faceter-agent
+sleep 10s
+done
